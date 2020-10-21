6e40f079950409ed409416b19b71cdbfdef6c950
Abdulhakeem <somlutions@gmail.com>
2020-10-20 19:15:52 -0700

    Added all random variables

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -20,16 +20,22 @@ function writePassword() {
   //All of my variables for my functions to access
   let upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
   let lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
-  let number = [0,1,2,3,4,5,6,7,8,9]
-  let special = [".","!","@","#","$","%","^","&","*","(",")","_","+","-","="]
+  let number = [0,1,2,3,4,5,6,7,8,9];
+  let special = [".","!","@","#","$","%","^","&","*","(",")","_","+","-","="];
 
   randomUpper = Math.floor(Math.random() * upperCase.length);
   upperIndex = upperCase[randomUpper];
 
-  randomLower = Math.floor(Math.random() * upperCase.length);
-  lowerIndex = upperCase[randomLower];
+  randomLower = Math.floor(Math.random() * lowerCase.length);
+  lowerIndex = lowerCase[randomLower];
 
-  console.log(upperIndex);
+  randomNumber = Math.floor(Math.random() * number.length);
+  numberIndex = number[randomNumber];
+
+  randomSpecial = Math.floor(Math.random() * special.length);
+  specialIndex = special[randomSpecial];
+
+  console.log(upperIndex, lowerIndex, numberIndex, specialIndex);
 
   function generatePassword(passwordLength, wantUpperCase, wantLowerCase, wantNumber, wantSpecial){
 
