8514ee6af2b112bc190075bae39178fa48b966b3
Abdulhakeem <somlutions@gmail.com>
2020-10-20 18:19:35 -0700

    Function scope doesn't allow for variables to be called within itself, removed from within generate function

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -8,7 +8,6 @@ function writePassword() {
 
   passwordText.value = password;
 
-  function generatePassword(){
   // I want all of my variables here
   const passwordLength = prompt("Please input how long you want your password to be, from 8 to 128 characters. Input only a number!");
 
@@ -23,6 +22,9 @@ function writePassword() {
   let lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
   let number = [0,1,2,3,4,5,6,7,8,9]
   let special = [".","!","@","#","$","%","^","&","*","(",")","_","+","-","="]
+
+  function generatePassword(){
+
 
 
 
