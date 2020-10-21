192df7f0949c81d6fde3ce224e4725b5dd9ab32c
Abdulhakeem <somlutions@gmail.com>
2020-10-20 18:09:29 -0700

    Changed constants to variables because I will be changing them. Added confirms

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -11,10 +11,20 @@ function writePassword() {
   function generatePassword(){
   // I want all of my variables here
   const passwordLength = prompt("Please input how long you want your password to be, from 8 to 128 characters. Input only a number!");
-  const upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
-  const lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
-  const number = [0,1,2,3,4,5,6,7,8,9]
-  const special = [".","!","@","#","$","%","^","&","*","(",")","_","+","-","="]
+
+  // All of my prompts and confirmations for boolean values
+  const wantUpperCase = confirm("Do you want UpperCase Letters in your password?");
+  const wantLowerCase = confirm("Do you want LowerCase Letters in your password?");
+  const wantNumber = confirm("Do you want Numbers in your password?");
+  const wantSpecial = confirm("Do you want Special Letters in your password?");
+
+  //All of my variables for my functions to access
+  let upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
+  let lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
+  let number = [0,1,2,3,4,5,6,7,8,9]
+  let special = [".","!","@","#","$","%","^","&","*","(",")","_","+","-","="]
+
+
 
   }
 
