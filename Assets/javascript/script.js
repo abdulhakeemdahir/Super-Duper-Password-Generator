7006ce7d8c1f300ce54f22cc32ce288c6e14d214
Abdulhakeem <somlutions@gmail.com>
2020-10-20 18:57:30 -0700

    Set up my first array randomizer

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -8,14 +8,14 @@ function writePassword() {
 
   passwordText.value = password;
 
-  // I want all of my variables here
-  const passwordLength = prompt("Please input how long you want your password to be, from 8 to 128 characters. Input only a number!");
+  // // I want all of my variables here
+  // const passwordLength = prompt("Please input how long you want your password to be, from 8 to 128 characters. Input only a number!");
 
-  // All of my prompts and confirmations for boolean values
-  const wantUpperCase = confirm("Do you want UpperCase Letters in your password?");
-  const wantLowerCase = confirm("Do you want LowerCase Letters in your password?");
-  const wantNumber = confirm("Do you want Numbers in your password?");
-  const wantSpecial = confirm("Do you want Special Letters in your password?");
+  // // All of my prompts and confirmations for boolean values
+  // const wantUpperCase = confirm("Do you want UpperCase Letters in your password?");
+  // const wantLowerCase = confirm("Do you want LowerCase Letters in your password?");
+  // const wantNumber = confirm("Do you want Numbers in your password?");
+  // const wantSpecial = confirm("Do you want Special Letters in your password?");
 
   //All of my variables for my functions to access
   let upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
@@ -23,9 +23,17 @@ function writePassword() {
   let number = [0,1,2,3,4,5,6,7,8,9]
   let special = [".","!","@","#","$","%","^","&","*","(",")","_","+","-","="]
 
-  function generatePassword(){
+  randomUpper = Math.floor(Math.random() * upperCase.length);
+  upperIndex = upperCase[randomUpper];
 
+  randomLower = Math.floor(Math.random() * upperCase.length);
+  lowerIndex = upperCase[randomLower];
 
+  console.log(upperIndex);
+
+  function generatePassword(passwordLength, wantUpperCase, wantLowerCase, wantNumber, wantSpecial){
+
+    randomUpper = Math.floor(Math.random)
 
 
   }
